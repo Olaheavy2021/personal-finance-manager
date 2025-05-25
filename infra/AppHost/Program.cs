975be47dbc2f9cd9fae0eb4129e71a035533826a
Sunkanmi Olawuwo <sunkanmiolawuwo.gas@gmail.com>
2025-05-25 19:15:34 +0100

    Patch: Set up the react project and add it to aspire

--- infra/AppHost/Program.cs
+++ infra/AppHost/Program.cs
@@ -1,5 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.PersonalFinanceManger_API>("personalfinancemanger-api");
+var pfmApi = builder.AddProject<Projects.PersonalFinanceManger_API>("personalfinancemanger-api")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("react", "../../frontend/pfm-app")
+    .WithReference(pfmApi)
+    .WaitFor(pfmApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
